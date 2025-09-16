18fa137e3f0b6370b0a60eb8e3f0a16100782b78
Sean Higgins Lim <sean_higgins_lim@dlsu.edu.ph>
2025-09-16 13:10:43 +0800

    Update AndroidSmallHome.kt. Remove an extra Glide and button relationship

--- app/src/main/java/com/thsst2/greenapp/AndroidSmallHome.kt
+++ app/src/main/java/com/thsst2/greenapp/AndroidSmallHome.kt
@@ -1,14 +1,11 @@
 package com.thsst2.greenapp
 import android.os.Bundle
-import android.view.View
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
 import android.widget.ImageView
-import androidx.activity.compose.setContent
 import com.bumptech.glide.Glide
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Text
 
 class AndroidSmallHome : AppCompatActivity() {
 	private var editTextValue1: String = ""
@@ -20,7 +17,6 @@ class AndroidSmallHome : AppCompatActivity() {
 		Glide.with(this).load("https://storage.googleapis.com/tagjs-prod.appspot.com/v1/5KZSjaV7Nf/wklyakqn_expires_30_days.png").into(findViewById<ImageView>(R.id.ra0p1lhf2i3g))
 		Glide.with(this).load("https://storage.googleapis.com/tagjs-prod.appspot.com/v1/5KZSjaV7Nf/qz8xcg2m_expires_30_days.png").into(findViewById<ImageView>(R.id.rjvarnxwuapq))
 		Glide.with(this).load("https://storage.googleapis.com/tagjs-prod.appspot.com/v1/5KZSjaV7Nf/b60kjme5_expires_30_days.png").into(findViewById<ImageView>(R.id.rlqd4vorx07s))
-		Glide.with(this).load("https://storage.googleapis.com/tagjs-prod.appspot.com/v1/5KZSjaV7Nf/bh4hn9xl_expires_30_days.png").into(findViewById<ImageView>(R.id.rlxbeos9oum8))
 		val editText1: EditText = findViewById(R.id.r0zz50xix97ik)
 		editText1.addTextChangedListener(object : TextWatcher {
 			override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -33,9 +29,9 @@ class AndroidSmallHome : AppCompatActivity() {
 				// after Text Changed
 			}
 		})
-		val button1: View = findViewById(R.id.rp0408m5ovlr)
-		button1.setOnClickListener {
-			println("Pressed")
-		}
+//		val button1: View = findViewById(R.id.rp0408m5ovlr)
+//		button1.setOnClickListener {
+//			println("Pressed")
+//		}
 	}
 }
